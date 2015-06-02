bcaf5c4e43255f7b84265e031dd6b687fa786d6c
angelfan <angelfan1314@163.com>
2015-06-03 00:23:07 +0800

    doc with slate

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module ApiRailsDemo
     config.autoload_paths << Rails.root.join('app/policies')
 
     # 为了测试我们把 limit 设置为 3
-    config.middleware.use Rack::RedisThrottle::Daily, max: 3
+    config.middleware.use Rack::RedisThrottle::Daily, max: 300
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
