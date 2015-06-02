a95e35e29b434715f66173ae0d156365ffa51214
angelfan <angelfan1314@163.com>
2015-06-03 00:32:19 +0800

    doc with slate

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/docs/index', to: 'docs#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
